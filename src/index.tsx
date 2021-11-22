5dc254aa0d27bfc399008f73194f2d9a6000963a
marie-ototoi <marie@ototoi.fr>
2021-11-22 22:24:44 +0100

    feat: add cover and real tracks

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,59 @@ import Player from "./components/Player";
 import reportWebVitals from "./reportWebVitals";
 const tracks = [
   {
-    duration: 30,
-    url: "/tracks/texte_30.mp3",
+    duration: 326,
+    url: "/a_71/disque_1/tracks/01_Izdiucz.mp3",
+    name: "Izdiucz",
   },
   {
-    duration: 90,
-    url: "/tracks/texte_65.mp3",
+    duration: 298,
+    url: "/a_71/disque_1/tracks/02_Hig.mp3",
+    name: "Hig",
   },
   {
-    duration: 65,
-    url: "/tracks/texte_30.mp3",
+    duration: 244,
+    url: "/a_71/disque_1/tracks/03_MehrabanBash.mp3",
+    name: "Mehraban Bash",
+  },
+  {
+    duration: 386,
+    url: "/a_71/disque_1/tracks/04_Nebah.mp3",
+    name: "Nebah",
+  },
+  {
+    duration: 295,
+    url: "/a_71/disque_1/tracks/05_Oudische.mp3",
+    name: "Oudische",
+  },
+  {
+    duration: 250,
+    url: "/a_71/disque_1/tracks/06_IzdiuczRadioEdit.mp3",
+    name: "IzdiuczRadioEdit",
+  },
+  {
+    duration: 205,
+    url: "/a_71/disque_1/tracks/07_Ayn.mp3",
+    name: "Ayn",
+  },
+  {
+    duration: 288,
+    url: "/a_71/disque_1/tracks/08_Volkische.mp3",
+    name: "Izdiucz",
+  },
+  {
+    duration: 305,
+    url: "/a_71/disque_1/tracks/09_HijazOnD.mp3",
+    name: "HijazOnD",
   },
 ];
 
 ReactDOM.render(
   <React.StrictMode>
-    <Player tracks={tracks} />
+    <Player
+      tracks={tracks}
+      background="url('/a_71/cover/cover_71.jpg')"
+      side={395}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
