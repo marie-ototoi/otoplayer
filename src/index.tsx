919c0e45cc97f78b373f505a6b24187dac45f0f2
marie-ototoi <marie@ototoi.fr>
2021-11-23 22:28:27 +0100

    refactor: remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Player from "./components/Player";
 import reportWebVitals from "./reportWebVitals";
 const tracks = [
